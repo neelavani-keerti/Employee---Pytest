9fbcf55292255b88e02fa8783d8be31d5e735f3f
neelavani-keerti <keertineelavani@gmail.com>
2025-12-11 15:53:05 +0530

    save

--- test_employee.py
+++ test_employee.py
@@ -3,7 +3,7 @@ def test_employee_details():
     expected_output=(
         "Employee Name:Keerti\n"
         "Employee ID:E123\n"
-        "Department=Tech\n"
-        "Salary=98000\n"
+        "Department:Tech\n"
+        "Salary:98000\n"
     )
     assert employee_details("Keerti","E123","Tech",98000)== expected_output
